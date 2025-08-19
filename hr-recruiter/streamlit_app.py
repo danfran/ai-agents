c5efa4d5cfcfc65fa7311bbd2239655e566d2845
Daniele Francesconi <dfrancesconi12@gmail.com>
2025-08-19 15:02:26 +0100

    Add Streamlit ui.

---
+++ hr-recruiter/streamlit_app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+from vector_db import init_vector_store
+from recruiting_agent import create_recruiter_agent
+from graph import build_graph
+
+
+with st.sidebar:
+    gemini_api_key = st.text_input("Gemini API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/danfran/ai-agents/blob/main/hr-recruiter/streamlit_app.py)"
+
+st.title("🔎 HR assistant - Pre-screener for Candidates' CVs")
+uploaded_file = st.file_uploader("Upload an article", type=("pdf"))
+question = st.text_input(
+    "Ask about matching skills for a candidate",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question and not gemini_api_key:
+    st.info("Please add your Gemini API key to continue.")
+
+if uploaded_file and question and gemini_api_key:
+    init_vector_store()
+    agent = create_recruiter_agent()
+    graph = build_graph()
+
+    article = uploaded_file.read().decode()
+
+    response = graph.invoke({'messages': [('user', prompt)]})
+    st.write("### Answer")
+    st.write(response.completion)
+
+    # json_loads = json.loads(response['messages'][-1].content)
+    # print('-' * 60)
+    # print(f"Name: {json_loads.get('name', 'Unknown')}\n")
+    # print(f"Candidate CV Summary: {json_loads.get('candidate_cv_summary', [])}\n")
+    # print(f"Matched skills: {', '.join(json_loads.get('matched_skills', []))}\n")
+    # print(f"Non matched skills: {', '.join(json_loads.get('non_matched_skills', []))}\n")
+    # print(f"Matched skills percentage: {json_loads.get('matched_skills_percentage', 0)}%")
+    # print('-' * 60)
--- hr-recruiter/vector_db.py
+++ hr-recruiter/vector_db.py
@@ -25,28 +25,37 @@ def split_documents(documents):
 
 # preload docs
 def init_vector_store():
-    directory_path = os.getenv('BASE_DOCUMENTS')
-
-    docs = []
-    for filename in os.listdir(directory_path):
-        if filename.lower().endswith('.pdf'):
-            print(f"Loading file: {filename}")
-            parsed_file = parse_pdf(os.path.join(directory_path, filename))
-            docs.append(parsed_file)
-
-    chunked_documents = split_documents(docs)
-
     embeddings_gg = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
     global vector_store
     vector_store = InMemoryVectorStore(embeddings_gg)
 
-    vector_store.add_documents(chunked_documents)
+    chunked_documents = get_existing_docs()
+    if chunked_documents:
+        vector_store.add_documents(chunked_documents)
+
     retriever = vector_store.as_retriever(
         search_type='similarity',
         search_kwargs={'k': 5},
     )
 
     return retriever
+
+
+def get_existing_docs():
+    directory_path = os.getenv('BASE_DOCUMENTS')
+    chunked_documents = None
+
+    if directory_path:
+        docs = []
+        for filename in os.listdir(directory_path):
+            if filename.lower().endswith('.pdf'):
+                print(f"Loading file: {filename}")
+                parsed_file = parse_pdf(os.path.join(directory_path, filename))
+                docs.append(parsed_file)
+        chunked_documents = split_documents(docs)
+
+    return chunked_documents
+
 
 @tool(response_format="content_and_artifact")
 def retrieve(query: str):
